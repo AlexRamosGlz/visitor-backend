ef1b696c07a6058a027fb18fe5e92c56c1655919
AlexRamosGlz <joralexfx@gmail.com>
2025-05-29 18:08:48 -0600

    mysql-entrypoin modified

--- mysql-entrypoint.sh
+++ mysql-entrypoint.sh
@@ -6,20 +6,28 @@
 #
 ##################################################################
 
-usage() {
-    echo "$(basename $0) -h HOST -u USER -p PASSWORD REMOTE_DB_NAME LOCAL_DB_NAME"
-}
-
-copy_snapshot() {
-    mysqldump -h source_MySQL_DB_instance_endpoint \
-    -u $USER \
-    -p $PASSWORD \
-    --port=3306 \
-    --single-transaction \
-    --routines \
-    --triggers \
-    --databases $REMOTE_DB_NAME $LOCAL_DB_NAME > $PATH
-}
-
 PATH="/var/lib/mysql"
 
+copy_snapshot() {
+    local HOST=rds
+    local USER=admin
+    local PASSWORD=12345
+    local PORT=3306
+    local REMOTE_DB_NAME=remote_test
+    local LOCAL_DB_NAME=local_test
+
+    mysqldump -h $HOST \
+        -u $USER \
+        -p $PASSWORD \
+        --port=3306 \
+        --single-transaction \
+        --routines \
+        --triggers \
+        --databases $REMOTE_DB_NAME $LOCAL_DB_NAME > $PATH
+}
+
+#copy_snapshot
+
+echo "$0 Initiated...."
+
+exec $@
