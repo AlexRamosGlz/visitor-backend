f5cfcf3d155c7e8ccc7906abe230789038803769
AlexRamosGlz <joralexfx@gmail.com>
2026-06-19 07:39:50 -0600

    feat(Migracion ORM): Migracion de ORM iniciada

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 import "reflect-metadata";
-import { AppDataSource } from "./data-source"
-import { Count } from "@entities/Count"
-import express from "express"
-import router from "@routes/router"
-import cors from "cors"
+import express from "express";
+import router from "@routes/router";
+import cors from "cors";
 
-AppDataSource.initialize().then(async () => {
-    const app = express()
+const app = express();
 
-    app.use(cors())
-    app.use(express.json())
-    app.use('/api', router)
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
-    app.listen(process.env.API_PORT || 8000, () => {
-        console.log("Server is running on port " + (process.env.API_PORT || 8000))
-    })
-
-}).catch((error: Error) => {
-    console.log(error)
-})
+app.listen(process.env.API_PORT || 8000, () => {
+  console.log("Server is running on port " + (process.env.API_PORT || 8000));
+});
--- src/services/Count.services.ts
+++ src/services/Count.services.ts
@@ -1,44 +1,29 @@
 import { ICount, ICountError } from "@interfaces/count";
-import { AppDataSource } from "../data-source"
 import { HTTP_STATUS_CODES } from "@lib/constants/httpStatus"
-import { Count } from "@entities/Count"
 import fs from 'fs'
-import crypto from 'crypto';
+import { UUID } from 'crypto';
+import { prisma } from "db/prisma";
 export class CountServices {
-
-    private respository = AppDataSource.getRepository(Count)
+    
     async getCount(ipAddress: string): Promise<ICount | ICountError> {
-        const countEntity = await this.respository.findOneBy({ ipAddress: ipAddress })
 
-        if(!countEntity) {
-            return {
-                message: "Count not found",
-                code: HTTP_STATUS_CODES.NOT_FOUND
-            }
-        }
+        prisma.count.findUnique({
+            where 
+        })
+        
 
-        return {
-            id: countEntity.id,
-            count: countEntity.count
+                return {
+            id: 1,
+            count: 1
         }
     }
 
     async updateCount(ipAddress: string): Promise<ICount | ICountError> {
-        const countEntity = await this.respository.findOneBy({ ipAddress: ipAddress})
         fs.writeFileSync('count.txt', this.getGlobalCount.toString())
 
-        if(!countEntity) {
-            return {
-                message: "Count not found",
-                code: HTTP_STATUS_CODES.NOT_FOUND
-            }
-        }
-
-        countEntity.count = countEntity.count + 1;
-        await this.respository.save(countEntity)
         return {
-            id: countEntity.id,
-            count: countEntity.count
+            id: 1,
+            count: 1
         }
     }
 
